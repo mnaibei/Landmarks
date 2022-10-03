b5a6b7f40cf8cf80ac6151fb7cf5be8eba6b99bd
mnaibei <muchajulius@gmail.com>
2022-10-03 17:04:04 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mucha Naibei on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
